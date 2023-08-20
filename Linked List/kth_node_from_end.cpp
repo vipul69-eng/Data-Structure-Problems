2b9e2dca5ee0f9abbda893c139ab9a34aaa1d866
Vipul Sharma <sharmavipul01002@gmail.com>
2023-08-20 11:50:01 +0530

    Create kth_node_from_end.cpp

---
+++ Linked List/kth_node_from_end.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+int length(Node *head)
+{
+    Node *current = head;
+    int len = 0;
+    if (!current)
+        return len;
+    while (current)
+    {
+        len += 1;
+        current = current->next;
+    }
+    return len;
+}
+
+Node *kth_from_end(Node *head, int position)
+{
+    int k = (length(head) - position) - 1;
+    Node *current = head;
+    if (k == 0)
+    {
+        return head;
+    }
+    while (--k && current)
+    {
+        current = current->next;
+    }
+    return current->next;
+}
